9b484f34f90ff7a301f3fd0ecd3f06abbc4bae13
Daniel Atkinson <hourd.tasa@gmail.com>
2015-08-03 19:33:38 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,38 @@
+# Cordova application for playign with BLE
+
+## Requirements
+
+This application requires nodejs installed
+
+It will also requires build tools for your intended mobile platform:
+
+- For Android it is the android SDK or android studio
+- Windows phone will require Visual Studio and the phone sdk
+
+## Installation
+
+Just install the node dependancies via:
+
+```bash
+npm install
+```
+
+This will also build the project into the www folder
+
+## Development
+
+Never change anything in the www folder as it will get deleted when the application is built via gulp.
+
+All the code changes should be made inside the app folder.
+
+To build just run:
+
+```bash
+npm run build
+```
+
+You can also set gulp to watch the app folder and auto-build when anyhting is changed.  This can be started via:
+
+```bash
+npm run watch
+
---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    rimraf = require('gulp-rimraf'),
+    ignore = require('gulp-ignore'),
+    concat = require('gulp-concat'),
+    newer = require('gulp-newer'),
+    plumber = require('gulp-plumber');
+
+gulp.task('styles', function() {
+  return gulp.src('app/less/app.less')
+    .pipe(plumber())
+    .pipe(less({ style: 'expanded' }))
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(gulp.dest('www/styles'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('www/styles'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(['app/scripts/default.js',
+    ])
+    .pipe(plumber())
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('www/scripts'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('www/scripts'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('app/assets/img/**/*')
+    .pipe(plumber())
+    .pipe(newer('app/assets/img/**/*'))
+    .pipe(gulp.dest('www/assets/img'));
+});
+
+gulp.task('html', function () {
+  return gulp.src('app/html/**/*')
+    .pipe(plumber())
+    .pipe(newer('app/html/**/*'))
+    .pipe(gulp.dest('www'));
+});
+
+gulp.task('clean', function() {
+  return gulp.src(['www/styles', 'www/scripts', 'www/assets/img'], { read: false })
+    .pipe(rimraf());
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images', 'html');
+});
+
+gulp.task('watch', function() {
+  gulp.watch('app/less/**/*.less', ['styles']);
+  gulp.watch('app/scripts/**/*.js', ['scripts']);
+  gulp.watch('app/assets/img/**/*', ['images']);
+  gulp.watch('app/html/**/*', ['html']);
+});
+
+gulp.task('watch-styles', function() {
+  gulp.watch('app/less/**/*.less', ['styles']);
+});
+
+gulp.task('watch-scripts', function() {
+  gulp.watch('app/scripts/**/*.js', ['scripts']);
+});
+
+gulp.task('watch-html', function() {
+  gulp.watch('app/html/**/*', ['html']);
+});
+
+gulp.task('watch-images', function() {
+  gulp.watch('app/assets/img/**/*', ['images']);
+});
